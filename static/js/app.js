d49ed97d93defde98dd4f3a6e90bf3b56de1ad5f
Tony Wang <tonywang3571@gmail.com>
2022-02-01 09:42:44 -0600

    Updated code with button to filter with a click and to load data when page loads

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
 // import the data from data.js
 const tableData = data;
 
-// Reference the HTML table using d3
+// Reference the HTML table using d3 (Data-Driven Document)
 var tbody = d3.select("tbody");
 
-//Create function to iterate through the list of data(object) and append into a table format.
+// Create function to iterate through the list of data(object) and append into a table format.
 function buildTable(data) {
     // First, clear out any existing data to prevent duplicates.
     tbody.html("");
@@ -22,4 +22,30 @@ function buildTable(data) {
             }
         );
     });   
+
+    // Function to filter data by date
+    function handleClick() {
+
+        // Grab the datetime value from the filter
+        let date = d3.select("#datetime").property("value");
+        let filteredData = tableData;
+
+        // Check to see if a date was entereed and filter data by date
+        if (date) {
+            // Appply 'filter' to data table to keep rows where
+            // 'datetime' value matches the filter value
+            filteredData = filteredData.filter(row => row.datetime === date);
+        };
+
+        // Rebuild the table using the filtered data
+        //@NOTE: if no date was entered, then filteredData will
+        // just be original tableData.
+        buildTable(filteredData);
+    };
+
+    // Attach an event to listen for the form button
+    d3.selectAll("#filter-btn").on("click", handleClick);
+
+    // Build the table when the page loads
+    buildTable(tableData);
 }
